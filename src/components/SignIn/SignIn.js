760a42c9996b0dd32f2d9b18b8404bb204d67f8b
maksLazarchuk <maks.lazarchyk@gmailcom>
2021-02-15 15:52:23 +0200

    sign-in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,9 +1,21 @@
 import "./SignIn.css";
+import React from 'react';
+import { Button } from '@material-ui/core';
 
 function SignIn() {
-  return <div className="sign-in">
-    <div className="admittance"></div>
-  </div>;
+  return (
+    <div className="sign-in">
+      <div className="admittance">
+        <div className="box-input">
+          <input type="Email" name="Email" placeholder="Email" />
+          <input type="Password" name="Password" placeholder="Password" />
+        </div>
+        <Button className="enter-button" variant="contained" color="primary">
+          Увійти
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
