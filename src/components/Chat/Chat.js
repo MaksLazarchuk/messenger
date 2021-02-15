2190609c83dce4792fcdddde0dc218cc63d5c163
maksLazarchuk <maks.lazarchyk@gmailcom>
2021-02-15 14:35:51 +0200

    project

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,7 @@
+import "./Chat.css";
+
+function Chat() {
+  return <div className="Chat"></div>;
+}
+
+export default Chat;
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,9 @@
+import "./SignIn.css";
+
+function SignIn() {
+  return <div className="sign-in">
+    <div className="admittance"></div>
+  </div>;
+}
+
+export default SignIn;
