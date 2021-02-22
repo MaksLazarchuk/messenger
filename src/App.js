329861be2c3126d1159535eb86b0f61f691bcd3b
maksLazarchuk <maks.lazarchyk@gmailcom>
2021-02-22 19:54:20 +0200

    sign-in work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import SignIn from "./components/SignIn/SignIn";
+import Chat from "./components/Chat/Chat";
 import "./App.css";
 
 function App() {
+  const [login, setLogin] = useState(false);
+
   return (
     <div className="App">
-      <SignIn />
+      {!login && <SignIn setLogin={setLogin} />}
+      {login && <Chat />}
     </div>
   );
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,16 +1,65 @@
+import { useState } from "react";
 import "./SignIn.css";
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React from "react";
+import { Button } from "@material-ui/core";
 
-function SignIn() {
+function SignIn({ setLogin }) {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const Submit = () => {
+    fetch("http://localhost:3030/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.status);
+        if (!data.token) {
+          throw new Error();
+        }
+        setLogin(true);
+        localStorage.setItem("token", data.token);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(`${error}`);
+      });
+  };
+
   return (
     <div className="sign-in">
       <div className="admittance">
         <div className="box-input">
-          <input type="Email" name="Email" placeholder="Email" />
-          <input type="Password" name="Password" placeholder="Password" />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="Email"
+            name="Email"
+            placeholder="Email"
+            required
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="Password"
+            name="Password"
+            placeholder="Password"
+            required
+          />
         </div>
-        <Button className="enter-button" variant="contained" color="primary">
+        <Button
+          onClick={Submit}
+          className="enter-button"
+          variant="contained"
+          color="primary"
+        >
           Увійти
         </Button>
       </div>
